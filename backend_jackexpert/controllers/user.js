ac50d3e38ba317faaaa9217ecc65702b6aa1925c
KaiCode <kaiquemedeiros440@gmail.com>
2024-09-10 22:55:33 -0300

    JWT integrado com sucesso!

--- backend_jackexpert/controllers/user.js
+++ backend_jackexpert/controllers/user.js
@@ -2,17 +2,27 @@
 
 import { db } from '../db.js'
 import bcrypt from 'bcrypt'
-
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config()
 const saltRound = 10
 
 const loginUser = (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
+    const {email, password} = req.body
     db.query("SELECT * FROM taskmaneger_db.users WHERE email = ?", [email], (err, result) => {
         if (err) return res.json(err)
         if (result.length > 0) {
             bcrypt.compare(password, result[0].password, (err, result)=>{
-                if (result) {res.send({msg: "Usuário logado com sucesso!", return: true})}
+                if (result) {
+                    const secret = process.env.SECRET
+
+                    const token = jwt.sign(
+                        {id: user.id},
+                        secret,
+                        { expiresIn: '1h'}
+                    )
+                    res.send({msg: "Usuário logado com sucesso!", token, return: true
+                    })}
                 else {res.send({msg: "A senha está incorreta."})}
             })
         } else {
@@ -23,8 +33,11 @@ const loginUser = (req, res) => {
 const registerUser = (req, res) => {
     const { name, email, password, confirmPassword } = req.body
     const dateToday = new Date().toISOString().split('T')[0]
-    if (!name || !email || !password || !confirmPassword || (password !== confirmPassword)){
-        return res.status(422).send({msg: 'Insira todas as informações para se cadastrar!'})
+    if (!name || !email || !password || !confirmPassword){
+        return res.status(422).send({msg: 'Insira todas as informações para se cadastrar.'})
+    }
+    if ((password !== confirmPassword)) {
+        return res.status(422).send({msg: 'Senhas não coincidem.'})
     }
     
     db.query("SELECT * FROM taskmaneger_db.users WHERE email = ?", [email], (err, result)=>{
